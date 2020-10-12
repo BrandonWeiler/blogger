de932e08706a5eb54d2518be86fa3680a0b06c78
Debian <bitnami@ip-172-26-14-68.ec2.internal>
2020-10-12 03:45:12 +0000

    updated index.js in api to route to the correct parts

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,3 +2,8 @@ var express = require('express');
 var router = express.Router();
 var ctrlBlog = require('../controller/blog');
 
+router.get('/blog', ctrlBlog.blogList);
+router.get('/blog:blogid', ctrlBlog.blogReadOne);
+router.put('/blog', ctrlBlog.blogUpdateOne);
+router.post('/blog', ctrlBlog.blogCreate);
+router.delete('/blog', ctrlBlog.blogDeleteOne);
